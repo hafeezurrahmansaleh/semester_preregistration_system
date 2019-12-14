5f051b784c7e0aeaed330ddb478335a1ca483ca2
Subroto Karmokar <subroto35-1734@diu.edu.bd>
2019-12-14 23:58:59 +0600

    password change option added

--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 # decorators
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -51,3 +54,20 @@ class TeacherSignUpView(CreateView):
         form.save()
         # login(self.request, user)
         return redirect('account_login')
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('account_login')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'account/change_password.html', {
+        'form': form
+    })
