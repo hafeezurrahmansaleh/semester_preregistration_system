a37303dfdf103a082a60504c2f9e1340efcbd620
nagma-islam <nagma35-1724@diu.edu.bd>
2019-12-22 15:14:11 +0600

    Teacher panel added

--- teacher/views.py
+++ teacher/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from adminpanel.models import Courses
 from .models import *
 
 def teacherPanelHome(request):
-    return render(request, 'teacherpanel/tphome.html')
+    id = 1
+    teacher = TeacherInfo.objects.get(pk=id)
+    course = Courses.objects.all()
+    context = {
+        'teacher' : teacher,
+        'course' : course
+    }
+    return render(request, 'teacherpanel/tphome.html',context)
