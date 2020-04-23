e9f3c5f0d43a4a7c66cba322edacb8e7c2bbb14c
Md. Abu Saleh <52953167+hafeeezurrahmansaleh@users.noreply.github.com>
2020-04-23 19:46:52 +0600

    Update views.py

--- teacher/views.py
+++ teacher/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from adminpanel.models import Courses
 from student.models import StudentInfo
-from .models import *
+from .models import TeacherInfo
 
 def teacherPanelHome(request):
-
     teacher = TeacherInfo.objects.get(tEmail = request.user.email)
     studentInfo = StudentInfo.objects.filter(stAdvisor=teacher)
     course = Courses.objects.all()
@@ -14,22 +12,6 @@ def teacherPanelHome(request):
         'courses' : course,
         'teachers': teacher,
         'students' :studentInfo
-=======
-<<<<<<< HEAD
-from adminpanel.models import Courses
-=======
-from django.contrib.auth.decorators import login_required
-from users.decorators import teacher_required
->>>>>>> 832dd4ff9d6c127e8db85eb2ac401c9342f95669
-from .models import *
-
-def teacherPanelHome(request):
-    id = 1
-    teacher = TeacherInfo.objects.get(pk=id)
-    course = Courses.objects.all()
-    context = {
-        'teacher' : teacher,
-        'course' : course
->>>>>>> refs/remotes/origin/master
     }
+
     return render(request, 'teacherpanel/tphome.html',context)
